1a7df828547195afeceb71882463b5efeaa9da98
Matt Player <mattplayer@mac.com>
2020-02-27 09:53:35 +0000

    updated comment

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 class Main {
   public static void main(String[] args) {
     Display.log("Welcome to ConnectN");
-    // handle command line passing number of connects, defaulting to 4
+    // handle command line passing number of connects / human & computer, with
+    // defaults
     String conn = args.length > 0 ? args[0] : "4";
     String human = args.length > 1 ? args[1] : "1";
     String comp = args.length > 2 ? args[2] : "2";
